755d116ac77140781db1f113fd57b5768c1a79d1
Nic Bovee <nic@nicbovee.com>
2023-01-24 21:49:47 -0700

    feat: check for updates

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates';
+import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [update, setUpdate] = useState(null);
+
+  useEffect(() => {
+    async function updateApp() {
+      if (__DEV__) return;
+      const updates = await Updates.checkForUpdateAsync();
+      console.log({ updates });
+      setUpdate(updates);
+    }
+    updateApp();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {__DEV__ && <Text style={styles.title}>In Development Mode</Text>}
+      <Text>Update channel: {Updates.channel}</Text>
+      <Text>isEmbeddedLaunch: {Updates.isEmbeddedLaunch}</Text>
+      <Text>releaseChannel: {Updates.releaseChannel}</Text>
+      <Text>updateId: {Updates.updateId}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 10,
   },
 });
